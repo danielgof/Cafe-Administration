33faf45c4a8ad5f6c2c1ebbbb3a5fd8a6af53c77
danielgof <gofmandon@gmail.com>
2022-03-25 23:49:39 +0300

    this is it for today

--- server/analitycs/db.py
+++ server/analitycs/db.py
@@ -34,28 +34,13 @@ class Database_Clients:
     # print(cursor.fetchall())
 
 
-# result = Database_Clients("client", "postgres", "admin", "127.0.0.1", "5432")
-# # result.createConnection("client", "postgres", "admin", "127.0.0.1", "5432")
-# result.get_data()
-# # print(result.get_data())
-# # class Database_Orders:
 
 
 #   def __init__(self, database, user, password, host, port):
 #     self.connection = self.createConnection(database, user, password, host, port)
 
 
-#   def createConnection(self, database, user, password, host, port):
-
-#     connection = None
-#     try:
-#       connection = psycopg2.connect(
-#       database="orders", 
-#       user="postgres", 
-#       password="admin", 
-#       host="127.0.0.1", 
-#       port="5432"
-#       )
+#
 #     except psycopg2.OperationalError as e:
 #       print('Unable to connect!\n{0}').format(e)
 
