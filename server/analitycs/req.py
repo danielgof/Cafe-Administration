bd8b03c4875ec9fb2e935c81573d2ca4971e03ea
danielgof <gofmandon@gmail.com>
2022-03-26 11:13:07 +0300

    fullstack?

--- server/analitycs/req.py
+++ server/analitycs/req.py
@@ -5,8 +5,3 @@ class Request:
     def get_data():
         result = requests.get('http://localhost:8080/api/v1/client')
         return result.json()
-
-# result = requests.get('http://localhost:8080/api/v1/client')
-
-# res = Request.get_data()
-# print(res)
