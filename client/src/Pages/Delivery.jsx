98313507b34a145fc995e399aa2892666f76fb76
Daniil Gofman <gofmandon@gmail.com>
2022-05-17 21:04:18 +0300

    will it work?

--- client/src/Pages/Delivery.jsx
+++ client/src/Pages/Delivery.jsx
@@ -1,40 +1,13 @@
 import React, { Component, useState } from 'react';
 import axios from 'axios';
 
+
 export default class Delivery extends Component {
-  const [inputValue, setInputvalue] = useState([]);
+  
   render() {
     return (
       <div>
-      <input value={inputValue} onChange={e => setInputValue(e.target.value)} />
-      <p>{inputValue}</p>
-      <button
-        disabled={inputValue.length === 0}
-        type="button"
-        onClick={async () => {
-          console.log(inputValue);
-          try {
-            await axios({
-              url: "https://google.com/",
-              headers: {
-                "Content-type": "application/json"
-              },
-              params: {
-                field: inputValue
-              },
-              method: "GET",
-              data: null
-            }).then(({ data }) => {
-              return data;
-            });
-          } catch (e) {
-            console.log("Sending error", e);
-          }
-        }}
-        style={{ padding: 20, backgroundColor: "red", cursor: "pointer" }}
-      >
-        Send to ....
-      </button>
+      <h6> </h6>
     </div>
     )
   }
