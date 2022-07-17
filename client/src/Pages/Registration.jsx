692a797322b4bf117bb9228daa1d02a4f98fbae4
Daniil Gofman <gofmandon@gmail.com>
2022-07-17 19:00:39 +0300

    Reg

--- client/src/Pages/Registration.jsx
+++ client/src/Pages/Registration.jsx
@@ -5,34 +5,52 @@ import "./Registration.css"
 
 
 const Registration = () => {
-  const [login, setName] = useState("");
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [mobileNumber, setMobileNumber] = useState("");
   const [message, setMessage] = useState("");
 
   let handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      let res = await fetch("https://httpbin.org/post", {
+    const body = JSON.stringify({
+      login: login,
+      password: password,
+      email: email
+    })
+
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "Content-Length": JSON.stringify(body).length
+    })
+      console.log(JSON.stringify({
+        login: login,
+        password: password,
+        email: email
+      }))
+      let res = await fetch("http://localhost:8081/register", {
         method: "POST",
-        body: JSON.stringify({
-          name: login,
-          email: email,
-          mobileNumber: mobileNumber,
-        }),
+        mode: "cors",
+        headers: headers,
+        body: body
       });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setName("");
+      // let resJson = await res.json();
+      if (res.status === 200) 
+      {
+        setLogin("");
+        setPassword("");
         setEmail("");
         setMessage("User created successfully");
-      } else {
+      } 
+      else 
+      {
         setMessage("Some error occured");
       }
     } catch (err) {
       console.log(err);
     }
   };
+
 
   return (
     <div className="App">
@@ -41,20 +59,20 @@ const Registration = () => {
         <input
           type="text"
           value={login}
-          placeholder="Name"
-          onChange={(e) => setName(e.target.value)}
+          placeholder="Login"
+          onChange={(e) => setLogin(e.target.value)}
+        />
+        <input
+          type="text"
+          value={password}
+          placeholder="Mobile Number"
+          onChange={(e) => setPassword(e.target.value)}
         />
         <input
           type="text"
           value={email}
           placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="text"
-          value={mobileNumber}
-          placeholder="Mobile Number"
-          onChange={(e) => setMobileNumber(e.target.value)}
         />
 
         <button type="submit">Create</button>
