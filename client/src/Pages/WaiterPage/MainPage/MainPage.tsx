7d2f4812be1f35c05d9576a3791710ae7fa81f4a
Daniil Gofman <gofmandon@gmail.com>
2023-01-04 15:25:40 +0300

    restaraunt map placeholder

--- client/src/Pages/WaiterPage/MainPage/MainPage.tsx
+++ client/src/Pages/WaiterPage/MainPage/MainPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../../Components/WaiterComponents/Header/Navbar';
+import './MainPage.css';
+import map from './plan.png';
 
 const MainPage = () => {
   return (
     <>
       <Header />
-      <p>Main Page</p>
+      <p className='waiter-main-page-title'>Main Page</p>
+      <img className='waiter-main-page-map' src={map}/>
     </>
   )
 }
