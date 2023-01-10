8e57418dff9d619ba7ddc2e9e2a7569b8614ea5d
Daniil Gofman <gofmandon@gmail.com>
2023-01-10 14:22:12 +0300

    add styles flixibility

--- client/src/Pages/Login/ManagerLogin/ManagerLogin.tsx
+++ client/src/Pages/Login/ManagerLogin/ManagerLogin.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import LoginManager from '../../../Components/Login/LoginManager/LoginManager';
+import './ManagerLogin.css';
 
 const ManagerLogin = () => {
   return (
     <>
       <LoginManager />
+      <a href='/'>
+        <button className='btn-back-login'>
+          Return home
+        </button>
+      </a>
     </>
   )
 }
--- client/src/Pages/Login/WaiterLogin/WaiterLogin.tsx
+++ client/src/Pages/Login/WaiterLogin/WaiterLogin.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import LoginWaiter from '../../../Components/Login/LoginWaiter/LoginWaiter';
+import './WaiterLogin.css'
 
 const WaiterLogin = () => {
   return (
     <>
       <LoginWaiter />
+      <a href='/'>
+        <button className='btn-back-login'>
+          Return home
+        </button>
+      </a>
     </>
   )
 }
